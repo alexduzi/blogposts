ab36e7589633bd77729fc1835447859f708dc43c
alexduzi <duzihd@gmail.com>
2025-05-27 16:05:24 -0300

    users entity/repository initial seeding

---
+++ src/main/java/com/alexduzi/blogposts/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.alexduzi.blogposts.repositories;
+
+import com.alexduzi.blogposts.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
